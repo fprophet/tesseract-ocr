7436810a3810dc78c1d46f215a8be77f1b4f1aa9
fprophet <eduard.ciuban96@gmail.com>
2024-05-20 13:05:57 +0300

    created a base structure for the web interface

---
+++ app/ocr.cpp
@@ -0,0 +1,33 @@
+
+// g++ ocr.cpp `pkg-config opencv4 --cflags --libs` -I/usr/local/include/opencv4 -L/usr/local/lib64 -llept -ltesseract -o ocr
+
+#include <opencv4/opencv2/opencv.hpp>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+int main()
+{
+    char *outText;
+
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+
+    // Open input image with leptonica library
+    Pix *image = pixRead("testocr.png");
+    api->SetImage(image);
+    // Get OCR result
+    outText = api->GetUTF8Text();
+    printf("OCR output:\n%s", outText);
+
+    // Destroy used object and release memory
+    api->End();
+    delete api;
+    delete [] outText;
+    pixDestroy(&image);
+
+    return 0;
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tesseract OCR</title>
+    <link rel="stylesheet" href="/resources/main.css?v=" <?= time()?>>
+</head>
+<body>
+    <header>
+        <div id="image-holder">
+            <img width="120" src="/resources/images/logo-image.jpg" alt="">
+            <span>Tesseract OCR</span>
+        </div>
+        <div id="nav-list"></div>
+    </header>
 
-echo "testing"
-?>
+    <section id="main">
+        <div id="upload-section">
+            <div id="input-wrapper">
+                <input type="file" id="upload">
+            </div>
+            <div id="image-preview">
+
+            </div>
+        </div>
+        <div id="processed-images-preview">
+            
+        </div>
+    </section>
+    
+    <footer></footer>
+</body>
+</html>
